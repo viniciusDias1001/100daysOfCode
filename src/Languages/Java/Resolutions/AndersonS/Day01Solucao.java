b4e299cdac7b061f0dfc0e6dc9aea062d1fbeaac
Anderson Soares <anderson.silva.04@outlook.com>
2023-06-05 03:52:02 -0300

    Day 01 Anderson

--- src/Languages/Java/Resolutions/AndersonS/Day01Solucao.java
+++ src/Languages/Java/Resolutions/AndersonS/Day01Solucao.java
@@ -1,4 +1,84 @@
 package src.Languages.Java.Resolutions.AndersonS;
 
+import java.util.*;
+
 public class Day01Solucao {
+
+    static private List<Integer> numerosPares = new ArrayList<>();
+    static private List<Integer> numerosImpares = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Digite a quantiade de números ");
+        int numeroRecebido = input.nextInt();
+
+        boolean margemDeNumeroTestada = testaMargemDeNumeros(numeroRecebido);
+
+        if (margemDeNumeroTestada == false){
+            numeroRecebido = validadorDeEntrada(numeroRecebido);
+        }
+
+        for ( int i=0 ;i < numeroRecebido;i++){
+
+            System.out.println("Entre com um número maior ou igual a 1 ");
+
+            int valorInteiroDaRodada = input.nextInt();
+            input.nextLine();
+
+            if (testaParidade(valorInteiroDaRodada)){
+                numerosPares.add(valorInteiroDaRodada);
+            }
+            else{
+                numerosImpares.add(valorInteiroDaRodada);
+            }
+        }
+        System.out.println(juntarListasParesImpares((ArrayList<Integer>) numerosPares, (ArrayList<Integer>) numerosImpares));
+    }
+    public static int validadorDeEntrada(int numeroRecebido){
+
+        Scanner input = new Scanner(System.in);
+
+        boolean numeroVerificado = testaMargemDeNumeros(numeroRecebido);
+
+        while(numeroVerificado == false){
+
+            System.out.println("número fora da margem permitida! digite um número entre 1 e 10000");
+
+            numeroRecebido = input.nextInt();
+            input.nextLine();
+            numeroVerificado = testaMargemDeNumeros(numeroRecebido);
+
+            if (numeroVerificado == false){
+                continue;
+            }
+        }
+        return numeroRecebido;
+    }
+    public static boolean testaMargemDeNumeros(int numero){
+        if (numero < 1 || numero>10000 ){
+            return false;
+        }
+        return true;
+    }
+    public static boolean testaParidade(int numero){
+        if (numero % 2 == 0){
+            return true;
+        }
+        return false;
+    }
+    public static ArrayList<Integer> ordenarPares(ArrayList<Integer> numerosPares){
+        Collections.sort(numerosPares);
+        return numerosPares;
+    }
+    public static ArrayList<Integer> ordenarImpares(ArrayList<Integer> numerosImpares){
+        Collections.sort(numerosImpares, Collections.reverseOrder());
+        return numerosImpares;
+    }
+    public static ArrayList<Integer> juntarListasParesImpares(ArrayList<Integer> pares, ArrayList<Integer> impares){
+       pares.addAll(impares);
+       // o final da lista de pares receberá os impares (objetivo de recebimento da lista final)
+       return pares;
+    }
 }
