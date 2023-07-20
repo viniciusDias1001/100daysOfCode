9e998262fedc524c0c40edfb967e515d476d2258
Carolina Batatinha <batatinha.carolina@gmail.com>
2023-07-20 14:52:32 -0300

    Adicionado svg e arquivos referentes à participação no desafio, bem como a resolução do primeiro exercício

---
+++ src/Languages/C#/Resolutions/CarolinaBatatinha/Day01Resolvido/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Digite o primeiro número inteiro: ");
+int X = Convert.ToInt32(Console.ReadLine());
+Console.Write("Digite o segundo número inteiro: ");
+int Y = Convert.ToInt32(Console.ReadLine());
+
+double divisao = X / Y;
+
+if (X % Y > 0)
+{
+    Console.Write("Divisão imposível");
+}
+else
+{
+    Console.Write($"{X} ÷ {Y} = {divisao}");
+}
