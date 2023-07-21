bd40cad82884422f91471deda5c9aeb3ff30360c
Carolina Batatinha <batatinha.carolina@gmail.com>
2023-07-21 10:16:18 -0300

    Resolução do dia 02

--- src/Languages/C#/Resolutions/CarolinaBatatinha/Day01Resolvido/Program.cs
+++ src/Languages/C#/Resolutions/CarolinaBatatinha/Day01Resolvido/Program.cs
@@ -1,15 +1,21 @@
-﻿Console.Write("Digite o primeiro número inteiro: ");
-int X = Convert.ToInt32(Console.ReadLine());
-Console.Write("Digite o segundo número inteiro: ");
-int Y = Convert.ToInt32(Console.ReadLine());
+﻿Console.Write("Informe quantos pares de valores inteiros serão lidos: ");
+int N = Convert.ToInt32(Console.ReadLine());
 
-double divisao = X / Y;
+for (int i = 0; i < N; i++)
+{
+    Console.Write("Digite o primeiro número inteiro: ");
+    int X = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Digite o segundo número inteiro: ");
+    int Y = Convert.ToInt32(Console.ReadLine());
 
-if (X % Y > 0)
-{
-    Console.Write("Divisão imposível");
-}
-else
-{
-    Console.Write($"{X} ÷ {Y} = {divisao}");
+    double divisao = (double)X / Y;
+
+    if (Y != 0)
+    {
+        Console.WriteLine($"\n{X} ÷ {Y} = {divisao:F1}");
+    }
+    else
+    {
+        Console.WriteLine("Divisão impossível");
+    }
 }
---
+++ src/Languages/C#/Resolutions/CarolinaBatatinha/Day02Resolvido/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("\nInforme a distância entre as motos X e Y em km: ");
+int k = Convert.ToInt32(Console.ReadLine());
+
+double tempo = (double)k * 2;
+
+Console.WriteLine($"O tempo necessário para a moto Y ficar {k}km distante da moto X é de {tempo} minutos.");
